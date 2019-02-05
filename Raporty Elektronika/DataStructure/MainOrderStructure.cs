8cb752308f4868fea4ea516d898fc35e8e2f4e74
Piotr Dąbrowski <piotrek816@wp.pl>
2019-02-05 15:20:43 +0100

    mm

--- Raporty Elektronika/DataStructure/MainOrderStructure.cs
+++ Raporty Elektronika/DataStructure/MainOrderStructure.cs
@@ -14,23 +14,38 @@ namespace Raporty_Elektronika.DataStructure
 
         }
 
-        
+
+        public static class SmtTab
+        {
+            public static bool checkBoxMst;
+            public static bool checkBoxLg;
+
+        }
 
         public DataTable sourceForDailyReport
         {
             get
             {
+                var filteredList = base.Smt.Select(o => o.Value);
+
+                if (!SmtTab.checkBoxLg)
+                {
+                    filteredList = filteredList.Where(o => o.clientGroup != "LG");
+                }
+                if (!SmtTab.checkBoxMst)
+                {
+                    filteredList = filteredList.Where(o => o.clientGroup != "MST");
+                }
 
                 var result = new DataTable();
 
-                Dictionary<DateTime, List<MST.MES.OrderStructureByOrderNo.SmtRecords>> dictByDay = base.Smt.SelectMany(o => o.Value.smtOrders).GroupBy(o => o.smtEndDate.Date).ToDictionary(x => x.Key, x => x.ToList());
-
-
+                Dictionary<DateTime, List<MST.MES.OrderStructureByOrderNo.SmtRecords>> dictByDay = filteredList.SelectMany(o=>o.smtOrders).GroupBy(o => o.smtEndDate.Date).ToDictionary(x => x.Key, x => x.ToList());
+                
 
                 return result;
             }
         }
-
+        
     }
     
 }
